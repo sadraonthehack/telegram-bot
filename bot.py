06a922d3599cfc54e5af92299c2ccce852eb0a53
sadraonthehack <sadra.marafe@gmail.com>
2026-06-24 18:12:13 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,14 +17,24 @@ API_ID = 27029926
 API_HASH = "6963d3bf5f8a776f5139d71cfc707abc"
 PHONE_NUMBER = "+989053716748"
 
-OWNERS = {"usernames": ["DevilWillCryBitch","MY_FALAH_M", "PV_KiTANAM","Pxcio","DevilWillCry1Bitch", "Pv_TERlYAKM"]}
+OWNERS = {"usernames": ["DevilWillCryBitch","MY_FALAH_M", "PV_KiTANAM","Pxcio","Closeyoureysi", "Pv_TERlYAKM"]}
 
 BOT_DIR = "downloads_bot1"
 if not os.path.exists(BOT_DIR):
     os.mkdir(BOT_DIR)
 
 files_defaults = {
-    "Kheshab.txt": "ONLINE",
+    "Kheshab.txt": "              .---.
+           '-.  |   |  .-'         
+             ___|   |___          
+        -=  [           ]  =-    
+            `---.   .---'         
+         __||__ |   | __||__      
+         '-..-' |   | '-..-'   
+           ||   |   |   ||     
+           ||_.-|   |-,_||     
+         .-"`   `"`'`   `"-.   
+    '                   '",
     "targetid.txt": "1",
     "Caption.txt": "",
     "time.txt": "2",
@@ -183,7 +193,18 @@ ping - Bot status
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
 **Coded by BrianMoser 
 ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
-**OWNER CHANNEL if you btw** → https://t.me/nahuhnothinghere/4
+                .---.
+           '-.  |   |  .-'         
+             ___|   |___          
+        -=  [           ]  =-    
+            `---.   .---'         
+         __||__ |   | __||__      
+         '-..-' |   | '-..-'   
+           ||   |   |   ||     
+           ||_.-|   |-,_||     
+         .-"`   `"`'`   `"-.   
+    .'                   '.
+
 """)
 
 @events.register(events.NewMessage(pattern=re.compile(r'^/setfwd (https?://t\.me/[^\s]+)$', re.IGNORECASE)))
